bf447bc0eda1b9dd42e8893fdbd246d643db3b60
Chinmaya Kelkar <chinmayakelkar18@gmail.com>
2015-10-07 13:30:12 -0500

    Login and registration system using volley App crash issue resolved by modifying getinstance function in AppController.
    Java lang error in main activity still to be resolved

---
+++ app/src/main/java/com/location_reminder/smarna/AppConfig.java
@@ -0,0 +1,12 @@
+package com.location_reminder.smarna;
+
+/**
+ * Created by Chinmaya on 06-Oct-15.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://smarna.net16.net/Login2.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://smarna.net16.net/Register2.php";
+}
